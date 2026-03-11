b0bd65eaeb4f17971eaeeb54bcfd6d0771e36d94
TheBestPessimist <cristian@tbp.land>
2026-03-11 20:35:14 +0200

    imdb clipper template

--- src/test-playwright/tests/imdb.spec.ts
+++ src/test-playwright/tests/imdb.spec.ts
@@ -9,7 +9,7 @@ test.describe('IMDB Templates', () => {
   test('Another Earth', async ({ extensionContext, extensionId }) => {
     const actual = await runHarTest(extensionContext, extensionId, {
       harPath: 'imdb/Another Earth.har',
-      templatePath: 'imdb-movie-clipper.json',
+      templatePath: 'imdb-clipper.json',
     });
     const expected = readExpected('imdb/Another Earth (2011).md');
     expectEqualsIgnoringNewlines(actual, expected);
@@ -18,7 +18,7 @@ test.describe('IMDB Templates', () => {
 	  test('Ponyo', async ({ extensionContext, extensionId }) => {
 	    const actual = await runHarTest(extensionContext, extensionId, {
 	      harPath: 'imdb/Ponyo.har',
-	      templatePath: 'imdb-movie-clipper.json',
+	      templatePath: 'imdb-clipper.json',
 	    });
 	    const expected = readExpected('imdb/Ponyo.md');
 	    expectEqualsIgnoringNewlines(actual, expected);
@@ -27,7 +27,7 @@ test.describe('IMDB Templates', () => {
   test('Andromeda', async ({ extensionContext, extensionId }) => {
     const actual = await runHarTest(extensionContext, extensionId, {
       harPath: 'imdb/Andromeda.har',
-	      templatePath: 'imdb-movie-clipper.json',
+	      templatePath: 'imdb-clipper.json',
     });
     const expected = readExpected('imdb/Andromeda (2000).md');
     expectEqualsIgnoringNewlines(actual, expected);
@@ -36,7 +36,7 @@ test.describe('IMDB Templates', () => {
   test('Shogun 1980', async ({ extensionContext, extensionId }) => {
     const actual = await runHarTest(extensionContext, extensionId, {
       harPath: 'imdb/Shogun 1980.har',
-	      templatePath: 'imdb-movie-clipper.json',
+	      templatePath: 'imdb-clipper.json',
     });
     const expected = readExpected('imdb/Shogun 1980.md');
     expectEqualsIgnoringNewlines(actual, expected);
@@ -45,7 +45,7 @@ test.describe('IMDB Templates', () => {
   test('Brooklyn Nine-Nine', async ({ extensionContext, extensionId }) => {
     const actual = await runHarTest(extensionContext, extensionId, {
       harPath: 'imdb/Brooklyn Nine-Nine.har',
-	      templatePath: 'imdb-movie-clipper.json',
+	      templatePath: 'imdb-clipper.json',
     });
     const expected = readExpected('imdb/Brooklyn Nine-Nine.md');
     expectEqualsIgnoringNewlines(actual, expected);
